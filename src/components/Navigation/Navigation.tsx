9db61fab3b0e8b49531d0a097f47281004c0ff58
Valerii Danylenko <devdanny.14@gmail.com>
2023-06-01 11:18:35 +0300

    initing the repo

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,13 @@
+export const Navigation: React.FC = () => {
+  return (
+    <>
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign Out
+        </p>
+      </nav>
+    </>
+  );
+};
